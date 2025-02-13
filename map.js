c0eb2b3f1d2c4f2ae26bf30baf904a5f664d16c4
norahkerendian <nokerendian@ucsd.edu>
2025-02-12 23:27:18 -0800

    map of municipalities created

--- map.js
+++ map.js
@@ -1,32 +1,43 @@
-// Set up the SVG canvas
-const width = 960, height = 600;
-const svg = d3.select("#map")
-    .attr("width", width)
-    .attr("height", height);
-
-// Define a projection and path generator
-const projection = d3.geoMercator()
-    .center([-74, 4])  // Center over Colombia
-    .scale(2000)       // Adjust scale as needed
-    .translate([width / 2, height / 2]);
-
-const path = d3.geoPath().projection(projection);
-
-// Load and render TopoJSON
-d3.json("colombia-municipalities.json").then(topology => {
-    const geojson = topojson.feature(topology, topology.objects.mpios);
-
-    svg.selectAll("path")
-        .data(geojson.features)
-        .enter().append("path")
-        .attr("class", "municipality")
-        .attr("d", path)
-        .on("mouseover", function (event, d) {
-            d3.select(this).style("fill", "#005b96");
-        })
-        .on("mouseout", function (event, d) {
-            d3.select(this).style("fill", "#b3cde0");
-        })
-        .append("title")  // Tooltip with municipality name
-        .text(d => d.properties.name);
+var map = L.map('map', {
+    center: [4.6097, -74.0817], 
+    zoom: 6,
+    minZoom: 6,
+    maxBounds: [[-6, -82], [15, -66]], 
+    maxBoundsViscosity: 1.0, 
+    attributionControl: false,
+    zoomControl: false
 });
+
+map.createPane('background');
+map.getPane('background').style.background = "black";
+
+var infoBox = document.getElementById('info-box');
+var infoBox1 = document.getElementById('info-box1');
+
+fetch('municipalities.json')
+    .then(response => response.json())
+    .then(data => {
+        var geoJsonLayer = L.geoJSON(data, {
+            style: function(feature) {
+                var ascii = feature.properties.ascii;
+                var color = ascii > 91 ? 'red' : 'blue';
+                return { fillColor: color, color: 'white', weight: 1 };
+            },
+            onEachFeature: function(feature, layer) {
+                layer.on('mouseover', function(e) {
+                    infoBox.textContent = feature.properties.NOMBRE_MPI; 
+                    layer.setStyle({ color: 'red', fillColor: 'red' }); 
+                });
+
+                layer.on('mouseout', function(e) {
+                    geoJsonLayer.resetStyle(layer); 
+                });
+            }
+        }).addTo(map);
+    });
+
+map.on('mousemove', function(e) {
+    infoBox1.innerHTML = e.latlng.lat.toFixed(6) + ', ' + e.latlng.lng.toFixed(6); 
+});
+
+
